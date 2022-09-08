603d92208adf6058ad5e4cb184db4c06528d73c3
lalitha2407 <lalitha@bootlabstech.com>
2022-09-08 18:11:03 +0530

    first

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "name" {
+    description = "ghsfdg"
+    type = string
+  
+}
+variable "region" {
+    description = "ghsfdg"
+    type = string
+  
+}
+variable "availability_zones" {
+    description = "ghsfdg"
+    type = set(string)
+  
+}
+variable "bucket" {
+    description = "ghsfdg"
+    type = string
+  
+}
+variable "instance_port" {
+    description = "ghsfdg"
+    type = number
+  
+}
+variable "instance_protocol" {
+    description = "ghsfdg"
+    type = string
+  
+}
+variable "lb_port" {
+    description = "ghsfdg"
+    type = number
+  
+}
+variable "lb_protocol" {
+    description = "ghsfdg"
+    type = string
+  
+}
---
+++ variables.tfvars
@@ -0,0 +1,8 @@
+name = "awsload"
+region = "ap-south-1"
+availability_zones = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+bucket = "store"
+instance_port = "8000"
+instance_protocol = "http"
+lb_port = "80"
+lb_protocol = "http"
