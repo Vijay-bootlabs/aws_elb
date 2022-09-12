ec6985d215d79a98a4e9010bf7bf6e2076fffc57
lalita247 <lalitalallu02@gmail.com>
2022-09-12 12:16:54 +0530

    fmt

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_elb" "abc" {
-    name = var.name
-    availability_zones = var.availability_zones
-  
-    access_logs {
-      bucket = var.bucket
-    }
-    listener {
+  name               = var.name
+  availability_zones = var.availability_zones
+
+  access_logs {
+    bucket = var.bucket
+  }
+  listener {
     instance_port     = var.instance_port
     instance_protocol = var.instance_protocol
     lb_port           = var.lb_port
     lb_protocol       = var.lb_protocol
-    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,40 +1,40 @@
 variable "name" {
-    description = "ghsfdg"
-    type = string
-  
+  description = "ghsfdg"
+  type        = string
+
 }
 variable "region" {
-    description = "ghsfdg"
-    type = string
-  
+  description = "ghsfdg"
+  type        = string
+
 }
 variable "availability_zones" {
-    description = "ghsfdg"
-    type = set(string)
-  
+  description = "ghsfdg"
+  type        = set(string)
+
 }
 variable "bucket" {
-    description = "ghsfdg"
-    type = string
-  
+  description = "ghsfdg"
+  type        = string
+
 }
 variable "instance_port" {
-    description = "ghsfdg"
-    type = number
-  
+  description = "ghsfdg"
+  type        = number
+
 }
 variable "instance_protocol" {
-    description = "ghsfdg"
-    type = string
-  
+  description = "ghsfdg"
+  type        = string
+
 }
 variable "lb_port" {
-    description = "ghsfdg"
-    type = number
-  
+  description = "ghsfdg"
+  type        = number
+
 }
 variable "lb_protocol" {
-    description = "ghsfdg"
-    type = string
-  
+  description = "ghsfdg"
+  type        = string
+
 }
